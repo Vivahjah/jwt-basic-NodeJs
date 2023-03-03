74796bcef035da23b7c30f356b5872ef49ce6fe0
Mengwa Ugo <mengwa44@yahoo.com>
2023-03-03 03:58:31 +0100

    added routes, and jwt

---
+++ routes/main.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { dashboard, login } = require('../controllers/main');
+const authenticationMiddleWare = require('../middleware/auth');
+const router = express.Router();
+
+
+
+router.route('/dashboard').get(authenticationMiddleWare, dashboard);
+router.route('/login').post(login)
+
+module.exports = router
